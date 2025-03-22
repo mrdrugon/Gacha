e26daced517340fe1002377126a41cfc5f7dcc0c
mrdrugon <93449253+mrdrugon@users.noreply.github.com>
2025-03-22 21:15:15 +0200

    Update Pack.java

--- card game/src/Pack.java
+++ card game/src/Pack.java
@@ -3,6 +3,8 @@ import java.util.List;
 import java.util.Random;
 
 public class Pack {
+    private static final Random rand = new Random();
+
     private static final Card[] COMMON_CARDS = {
             new Card("Red", 2, 3, "Common", false, false),
             new Card("Blue", 3, 2, "Common", false, false),
@@ -19,29 +21,29 @@ public class Pack {
             new Card("Rainbow", 6, 6, "Legendary", false, false)
     };
 
-    public static List<Card> openPack(){
+    private static final int COMMON_CHANCE = 600; // 60%
+    private static final int RARE_CHANCE = 990;   // 39%
+    private static final int PACK_SIZE = 5;
+
+    public static List<Card> openPack() {
         List<Card> pack = new ArrayList<>();
-        Random rand = new Random();
 
-        int commonChance = 600; //60%
-        int rareChance = 990; //39%
-
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             int chance = rand.nextInt(1000);
 
-            if (chance < commonChance){
-                pack.add(COMMON_CARDS[rand.nextInt(COMMON_CARDS.length)]);
-            } else if (chance < rareChance) {
-                pack.add(RARE_CARDS[rand.nextInt(RARE_CARDS.length)]);
+            if (chance < COMMON_CHANCE) {
+                pack.add(createNewCard(COMMON_CARDS));
+            } else if (chance < RARE_CHANCE) {
+                pack.add(createNewCard(RARE_CARDS));
             } else {
-                pack.add(LEGENDARY_CARDS[rand.nextInt(LEGENDARY_CARDS.length)]);
+                pack.add(createNewCard(LEGENDARY_CARDS));
             }
         }
         return pack;
     }
 
     private static Card createNewCard(Card[] cardPool) {
-        Card template = cardPool[new Random().nextInt(cardPool.length)];
-        return new Card(template.getName(), template.getAttack(), template.getHealth(), template.getRarity(), template.hasDoubleAttack(), template.hasRevive());
+        Card template = cardPool[rand.nextInt(cardPool.length)];
+        return template.createCopy();
     }
 }
