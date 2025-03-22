8e80be18902eb7b202ef5aff8f6ddaaf8a2eb433
mrdrugon <93449253+mrdrugon@users.noreply.github.com>
2025-03-22 21:13:47 +0200

    Update Deck.java

--- card game/src/Deck.java
+++ card game/src/Deck.java
@@ -3,34 +3,45 @@ import java.util.List;
 
 public class Deck {
     private ArrayList<Card> deck;
-    private static final int DECK_SIZE = 5;
+    public static final int DECK_SIZE = 5;
 
     public Deck() {
         this.deck = new ArrayList<>();
     }
-    public void addToDeck(Card card) throws Exception {
-        if (deck.size() < DECK_SIZE){
+
+    public List<Card> getCards(){
+        return deck;
+    }
+
+    public boolean addCard(Card card){
+        if (deck.size() < DECK_SIZE && !deck.contains(card)){
             deck.add(card);
+            return true;
         }
-        else {
-            Exception IndexOutOfBoundsException = new Exception("deck already empty!");
-            throw IndexOutOfBoundsException;
-        }
+        return false;
     }
 
-    public void removeFromDeck(int index) throws Exception {
-        if(index >= 0 && index < deck.size()){
-            deck.remove(index);
-        }
-        else {
-            Exception IndexOutOfBoundsException = new Exception("deck already empty!");
-            throw IndexOutOfBoundsException;
-        }
+    public boolean removeCard(int cardId){
+        return deck.removeIf(card -> card.getId() == cardId);
     }
 
-    public void setDeck(List<Card> selectedDeck){
+    public List<Card> getDeck(){
+        return new ArrayList<>(deck);
+    }
+
+    public boolean setDeck(List<Card> selectedDeck){
         if (selectedDeck.size() == DECK_SIZE){
             deck = new ArrayList<>(selectedDeck);
+            return true;
         }
+        return false;
+    }
+
+    public boolean isFull(){
+        return deck.size() == DECK_SIZE;
+    }
+
+    public void clearDeck(){
+        deck.clear();
     }
 }
